c0a3051106b42ff002db47f42929ca21c806e74e
morozovigor <perffectgame@gmail.com>
2015-10-29 10:01:47 +0300

    add some backend feauters

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+define('TM_DIR', get_template_directory(__FILE__));
+define('TM_URL', get_template_directory_uri(__FILE__));
+
+require_once TM_DIR . '/lib/Parser.php';
+
 function add_style(){
     wp_enqueue_style( 'my-bootstrap-extension', get_template_directory_uri() . '/css/bootstrap.css', array(), '1');
     wp_enqueue_style( 'my-styles', get_template_directory_uri() . '/css/style.css', array('my-bootstrap-extension'), '1');
@@ -13,10 +18,17 @@ function add_script(){
     wp_enqueue_script( 'my-bootstrap-extension', get_template_directory_uri() . '/js/bootstrap.js', array(), '1');
     wp_enqueue_script( 'my-script', get_template_directory_uri() . '/js/script.js', array(), '1');
     wp_enqueue_script( 'fotorama-js', get_template_directory_uri() . '/js/fotorama.js', array(), '1');
-
-    
 }
 
+function add_admin_script(){
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-2.1.3.min.js', array(), '1');
+    wp_enqueue_script('admin',get_template_directory_uri() . '/js/admin.js', array(), '1');
+    wp_enqueue_style( 'my-bootstrap-extension-admin', get_template_directory_uri() . '/css/bootstrap.css', array(), '1');
+    wp_enqueue_script( 'my-bootstrap-extension', get_template_directory_uri() . '/js/bootstrap.js', array(), '1');
+    wp_enqueue_style( 'my-style-admin', get_template_directory_uri() . '/css/admin.css', array(), '1');
+}
+
+add_action('admin_enqueue_scripts', 'add_admin_script');
 add_action( 'wp_enqueue_scripts', 'add_style' );
 add_action( 'wp_enqueue_scripts', 'add_script' );
 
